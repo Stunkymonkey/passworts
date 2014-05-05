1b50777da3caa5873bfefa978958d5fe67eb1827
CookingRobot <friedrich@cookingrobot.de>
2014-05-05 14:07:57 +0300

    Finished first design of main and result page

--- online/app/views.py
+++ online/app/views.py
@@ -13,7 +13,9 @@ def index():
 	form = Input()
 	if form.validate_on_submit():
 		global requested
-		requested = 'You passwords are ' + str(form.pw_length.data) + ' letters long,' +'and you are getting ' + str(form.pw_count.data) + ' passwords.'
+		requested = 'Here are your '+str(form.pw_count.data)+' passwords:'
+
+		#requested = 'You passwords are ' + str(form.pw_length.data) + ' letters long,' +'and you are getting ' + str(form.pw_count.data) + ' passwords.'
 		global password_ready
 		password_ready = (generator.generate(int(form.pw_length.data), int(form.pw_count.data)))
 		return redirect('/result')
