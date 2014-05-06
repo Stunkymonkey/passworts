26da23d4d6b12cf778764e38dc9d284bf182dd3c
CookingRobot <friedrich@cookingrobot.de>
2014-05-06 10:56:31 +0300

    updated error strings

--- online/app/views.py
+++ online/app/views.py
@@ -4,6 +4,7 @@ from app.forms import Input
 from app import generator
 
 
+
 @app.route('/index')
 def index():
 	return redirect('/')
@@ -14,28 +15,28 @@ def home():
 		try:
 			int(form.pw_length.data)
 		except ValueError:
-			flash("You have to enter a number")
+			flash("Please enter a real number")
 			return redirect('/')
 		try:
 			int(form.pw_count.data)
 		except ValueError:
-			flash("You have to enter a number")
+			flash("Please enter a real number")
 			return redirect('/')
 		if int(form.pw_length.data) <=4:
-			flash("It is not possible to create a password unter 5 letters!")
+			flash("It's not possible to create a password shorter than 5 letters!")
 			return redirect('/')
 		if int(form.pw_length.data) >16:
-			flash("It is not possible to create a password over 16 letters!")
+			flash("It's not possible to create a password longer than 16 letters!")
 			return redirect('/')
 		if int(form.pw_count.data) <=0:
-			flash("It is not possible to create no passwords!")
+			flash("Please create more than zero passwords")
 			return redirect('/')
 		if int(form.pw_count.data) >50:
 			flash("It is not possible to create more than 50 passwords!")
 			return redirect('/')
 		else:
 			global requested
-			requested = 'Here are your '+str(form.pw_count.data)+' passwords:'
+			#requested = 'Here are your '+str(form.pw_count.data)+' passwords:'
 			global password_ready
 			password_ready = (generator.generate(int(form.pw_length.data), int(form.pw_count.data)))
 			return redirect('/result')
