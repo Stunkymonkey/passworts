bd21de6927472433619e4ceb8bb753bbc566cf0a
Felix Buehler <account@buehler.rocks>
2017-06-20 19:19:36 +0200

    fixed deprecated warning from wtf-forms

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import BooleanField, TextField
 from wtforms.validators import Required
 
 
-class Input(Form):
+class Input(FlaskForm):
     pw_length = TextField('pw_length')
     pw_count = TextField('pw_count')
     random = BooleanField('random', default=False)
