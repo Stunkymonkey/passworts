a8c07b4a030f1b76f74456b75a67e194772fac43
Felix Buehler <account@buehler.rocks>
2022-11-06 17:24:14 +0100

    pylint fix passworts-web

--- passworts.py
+++ passworts.py
@@ -1,20 +1,51 @@
 #!/usr/bin/env python3
-
-from collections import defaultdict
+""" passworts flask webserver"""
 
 from flask import Flask, Response, flash, redirect, render_template, stream_with_context
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, IntegerField
+from wtforms.validators import InputRequired
 
 import generator
-from input import input_check
-from forms import Input
 
 
-def dd():
-    """
-    these lines are required, because pickle does not store the data type
-    https://stackoverflow.com/questions/27732354/unable-to-load-files-using-pickle-and-multipile-modules
-    """
-    return defaultdict(int)
+class Input(FlaskForm):
+    """flask forms"""
+
+    pw_length = IntegerField("pw_length", validators=[InputRequired()])
+    pw_count = IntegerField("pw_count", validators=[InputRequired()])
+    random_lenght = BooleanField("random", default=False)
+
+
+def input_check(form):
+    """flask input validation"""
+    input_correct = False
+    while input_correct is False:
+        if form.random_lenght.data is False:
+            try:
+                int(form.pw_length.data)
+            except ValueError:
+                flash("You have to enter a number")
+                break
+            if int(form.pw_length.data) <= 4:
+                flash("It is not possible to create a password unter 5 letters!")
+                break
+            if int(form.pw_length.data) > 16:
+                flash("It is not possible to create a password over 16 letters!")
+                break
+        try:
+            int(form.pw_count.data)
+        except ValueError:
+            flash("You have to enter a number")
+            break
+        if int(form.pw_count.data) <= 0:
+            flash("It is not possible to create no passwords!")
+            break
+        if int(form.pw_count.data) > 50:
+            flash("It is not possible to create more than 50 passwords!")
+            break
+        input_correct = True
+    return input_correct
 
 
 app = Flask("passworts")
@@ -23,20 +54,24 @@ app.config.from_object("config")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    """index page"""
     form = Input()
     return render_template("index.html", title="Home", form=form)
 
 
 @app.route("/result", methods=["POST"])
 def result():
+    """resulting page"""
+
     def stream_pw():
         yield render_template("result.html", title="Result")
         yield '\n<ul class="centeredList">\n'
         for _i in range(pw_count):
-            curr_pw = generator.generate(pw_length, random, "./dict/text.txt")
-            # print (curr_pw)
+            curr_pw = generator.generate(pw_length, random_lenght, "./dict/text.txt")
+            # print(curr_pw)
             yield (
-                f'<input class="result" type="text" value={curr_pw} readonly onclick="this.select();">\n'
+                f'<input class="result" type="text" value={curr_pw} readonly'
+                ' onclick="this.select();">\n'
             )
         yield "</ul>\n"
         yield "</div>\n"
@@ -44,8 +79,8 @@ def result():
     form = Input()
     if form.validate_on_submit():
         if input_check(form):
-            random = form.random.data
-            if not bool(random):
+            random_lenght = form.random_lenght.data
+            if not bool(random_lenght):
                 pw_length = int(form.pw_length.data)
             else:
                 pw_length = 0
@@ -56,18 +91,14 @@ def result():
 
 
 @app.route("/result", methods=["GET"])
-def result2():
-    return redirect("/")
-
-
-@app.route("/cancel", methods=["GET", "POST"])
-def cancel():
-    flash("The generation sould be canceled (unsure)!")
+def result_get():
+    """redirect get to results to index"""
     return redirect("/")
 
 
 @app.errorhandler(404)
 def page_not_found(_error):
+    """error page display"""
     return render_template("404.html", title="Error")
 
 
