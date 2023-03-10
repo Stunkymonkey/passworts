d8e1e652042cadc80c60a2ebf70674dc42302956
Felix Buehler <account@buehler.rocks>
2023-03-10 20:41:39 +0100

    nix-module: switch from gunicorn to uwsgi

--- module.nix
+++ module.nix
@@ -5,6 +5,43 @@ with lib;
 let
   cfg = config.services.passworts;
   pkg = cfg.package;
+  systemd-settings = {
+    User = "passworts";
+    DynamicUser = true;
+    Restart = "on-failure";
+
+    BindReadOnlyPaths = [
+      "${config.environment.etc."ssl/certs/ca-certificates.crt".source}:/etc/ssl/certs/ca-certificates.crt"
+      builtins.storeDir
+      "-/etc/resolv.conf"
+      "-/etc/nsswitch.conf"
+      "-/etc/hosts"
+      "-/etc/localtime"
+    ];
+    CapabilityBoundingSet = "";
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    PrivateDevices = true;
+    PrivateUsers = true;
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+
+    ProtectKernelTunables = true;
+    RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    SystemCallArchitectures = "native";
+    # gunicorn needs setuid
+    SystemCallFilter = [ "@system-service" "~@privileged" "@resources" "@setuid" "@keyring" ];
+    UMask = "0066";
+  } // lib.optionalAttrs (cfg.port < 1024) {
+    AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+    CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+  };
 in
 {
   options.services.passworts = {
@@ -31,56 +68,52 @@ in
   };
 
   config = mkIf cfg.enable {
-    systemd.services.passworts = {
-      description = "Passworts server";
+    systemd.services.passworts =
+      let
+        uwsgiConfig.uwsgi = {
+          type = "normal";
+          plugins = [ "python3" ];
+          module = "passworts";
 
-      serviceConfig = {
-        ExecStart = ''
-          ${pkg.python3.pkgs.gunicorn}/bin/gunicorn passworts
-        '';
-        Restart = "on-failure";
+          chdir = cfg.package;
+          http-socket = "${cfg.address}:${toString cfg.port}";
 
-        User = "passworts";
-        DynamicUser = true;
-
-        BindReadOnlyPaths = [
-          "${config.environment.etc."ssl/certs/ca-certificates.crt".source}:/etc/ssl/certs/ca-certificates.crt"
-          builtins.storeDir
-          "-/etc/resolv.conf"
-          "-/etc/nsswitch.conf"
-          "-/etc/hosts"
-          "-/etc/localtime"
-        ];
-        CapabilityBoundingSet = "";
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        PrivateDevices = true;
-        PrivateUsers = true;
-        ProtectClock = true;
-        ProtectControlGroups = true;
-        ProtectHome = true;
-        ProtectHostname = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        SystemCallArchitectures = "native";
-        # gunicorn needs setuid
-        SystemCallFilter = [ "@system-service" "~@privileged" "@resources" "@setuid" "@keyring" ];
-        UMask = "0066";
-      } // lib.optionalAttrs (cfg.port < 1024) {
-        AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
-        CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+          # master = true;
+          enable-threads = true;
+          processes = 4;
+          threads = 2;
+          vacuum = true;
+        };
+        uwsgiConfigFile = pkgs.writeText "uwsgi-passworts.json" (builtins.toJSON uwsgiConfig);
+      in
+      {
+        description = "Passworts server";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = systemd-settings // {
+          ExecStart = ''
+            ${pkgs.uwsgi.override { plugins = ["python3"]; }}/bin/uwsgi --json ${uwsgiConfigFile}
+          '';
+        };
+        environment = {
+          PYTHONPATH = "${pkg.python3.pkgs.makePythonPath pkg.propagatedBuildInputs}:${pkg}";
+        };
       };
 
-      wantedBy = [ "multi-user.target" ];
+    # sadly streaming does not work with gunicorn, therefor the uwsgi is used
+    # systemd.services.passworts = {
+    #   description = "Passworts server";
+    #   wantedBy = [ "multi-user.target" ];
 
-      environment = {
-        GUNICORN_CMD_ARGS = "--bind=${cfg.address}:${toString cfg.port} --worker-class=gevent";
-        PYTHONPATH = "${pkg.python3.pkgs.makePythonPath pkg.propagatedBuildInputs}:${pkg}";
-      };
-    };
+    #   serviceConfig = systemd-settings // {
+    #     ExecStart = ''
+    #       ${pkg.python3.pkgs.gunicorn}/bin/gunicorn passworts
+    #     '';
+    #   };
+
+    #   environment = {
+    #     GUNICORN_CMD_ARGS = "--bind=${cfg.address}:${toString cfg.port} --timeout 0 --worker-class=gevent";
+    #     PYTHONPATH = "${pkg.python3.pkgs.makePythonPath pkg.propagatedBuildInputs}:${pkg}";
+    #   };
+    # };
   };
 }
